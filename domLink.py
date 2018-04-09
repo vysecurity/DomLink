cd89d503780f65adf019e760c28356ad5dc4b5d6
vysec <vysec.private@gmail.com>
2018-04-09 22:57:48 +0100

    b4ldr refactor

--- domLink.py
+++ domLink.py
@@ -1,206 +1,207 @@
-import ConfigParser
-import argparse
-import os
-import sys
-import requests
-import json
-import urllib
+#!/usr/bin/env python
 
-parser = argparse.ArgumentParser(description='DomLink Discovery')
-parser.add_argument('-d', '--domain', help='Domain to perform DomLink Discovery on')
-parser.add_argument('-o', '--output', help='Output file')
-args = parser.parse_args()
-config = ConfigParser.RawConfigParser()
-config.read('domLink.cfg')
-api_key = config.get('API_KEYS', 'whoxy')
-
-VERSION = "0.1.1"
-
-company_names = []
-company_emails = []
-company_domains = []
-
-blocked_names = []
-blocked_emails = []
-
-bChanged = False
-iteration = 1
-
-def banner():
-	print "DomLink Domain Discovery Tool"
-	print "Author: Vincent Yiu (@vysecurity)"
-	print "https://www.github.com/vysec/DomLink"
-	print "Version: %s" % VERSION
-	print ""
-
-def enum(domain):
-	global bChange, iteration
-
-	bChanged = False
-	url = "http://api.whoxy.com/?key=%s&whois=%s" % (api_key,args.domain)
-	r = requests.get(url)
-	content = json.loads(r.text)
-
-	print "[*] Performing WHOIS lookup on %s" % args.domain
-
-	if (content["status"] != 1):
-		print "[!] WHOIS lookup failed, your API key is probably invalid or credits have been exhausted"
-
-	try:
-		if not (content["registrant_contact"]["company_name"] in company_names):
-			company_names.append(content["registrant_contact"]["company_name"])
-
-		if not (content["registrant_contact"]["email_address"] in company_emails):
-			company_emails.append(content["registrant_contact"]["email_address"])
-
-		if not (content["administrative_contact"]["company_name"] in company_names):
-			company_names.append(content["administrative_contact"]["company_name"])
-
-		if not (content["administrative_contact"]["email_address"] in company_emails):
-			company_emails.append(content["administrative_contact"]["email_address"])
-
-		if not (content["technical_contact"]["company_name"] in company_names):
-			company_names.append(content["technical_contact"]["company_name"])
-
-		if not (content["technical_contact"]["email_address"] in company_emails):
-			company_emails.append(content["technical_contact"]["email_address"])
-	except:
-		bChanged = True
-
-	print ""
-	print "-------------------"
-	print "[*] Unique Company names:"
-
-	for company_name in company_names:
-		print company_name
-
-	print ""
-	print "[*] Unique Company emails:"
-
-	for company_email in company_emails:
-		print company_email
-
-	print ""
-	bChange = True
-
-def expand():
-	global bChange,iteration
+from ConfigParser import RawConfigParser
+from argparse import ArgumentParser
+from requests import get
+from sys import exit
+import logging
 
 
-	print "[*] Performing Reverse WHOIS lookup"
+__version__ = '0.1.0'
 
-	# Company names first
-	
-	for company_name in company_names:
-		pages = 9999
-		cur_page = 1
-		
-		while (cur_page-1 < pages):
-			print "%s out of %s" %(cur_page, pages)
-			url = "http://api.whoxy.com/?key=%s&reverse=whois&company=%s&mode=mini&page=%s" % (api_key, urllib.quote(company_name), cur_page)
 
-			r = requests.get(url)
-			content = json.loads(r.text)
-			#print content
-			if content["status"] == 1:
-				pages = content["total_pages"]
-				for result in content["search_result"]:
-					try:
-						if not (result["domain_name"] in company_domains):
-							company_domains.append(result["domain_name"])
-						if not(result["company_name"] in company_names):
-							company_names.append(result["company_name"])
-						if not(result["email_address"].lower() in company_emails) and not(result["email_address"].lower() in blocked_emails):
-							bAdd = raw_input("[*] Do you want to add '%s' as a company email? (Y/n):" % result["email_address"]).upper()
-							if bAdd == "" or bAdd == "Y":
-								bAdd = "Y"
-							else:
-								bAdd = "N"
+def set_log_level(args_level):
+    log_level = logging.ERROR
+    if args_level == 1:
+        log_level = logging.WARN
+    elif args_level == 2:
+        log_level = logging.INFO
+    elif args_level > 2:
+        log_level = logging.DEBUG
+    logging.basicConfig(level=log_level)
 
-							if bAdd == "Y":
-								bChange = True
-								company_emails.append(result["email_address"].lower())
-							else:
-								blocked_emails.append(result["email_address"].lower())
 
-					except:
-						continue
-			cur_page += 1
+def get_args():
+    parser = ArgumentParser(description='''
+        DomLink Domain Discovery Tool
+        Author: Vincent Yiu (@vysecurity)
+        https://www.github.com/vysec/DomLink
+        Version: {}'''.format(__version__))
+    parser.add_argument('domain', help='Domain to perform DomLink Discovery on')
+    parser.add_argument('-o', '--output', help='Output file')
+    parser.add_argument('-A', '--api', help='https://www.whoxy.com API key')
+    parser.add_argument('-v', '--verbose', action='count')
+    parser.add_argument('-C', '--companies', action='store_true',
+            help='recersivly search companies')
+    parser.add_argument('-E', '--emails', action='store_true',
+            help='recersivly search emails')
+    parser.add_argument('-D', '--domains', action='store_true',
+            help='recersivly search domains')
+    return parser.parse_args()
 
-	print company_emails
-	print company_names
 
-	# Emails now
-	
-	for company_email in company_emails:
-		pages = 9999
-		cur_page = 1
-		
-		while (cur_page-1 < pages):
+def read_key_from_config():
+    config = RawConfigParser()
+    config.read('domLink.cfg')
+    return config.get('API_KEYS', 'whoxy')
 
-			print "%s out of %s" %(cur_page, pages)
- 			url = "http://api.whoxy.com/?key=%s&reverse=whois&email=%s&mode=mini&page=%s" % (api_key, urllib.quote(company_email), cur_page)
 
-			r = requests.get(url)
-			content = json.loads(r.text)
-			#print content
-			if content["status"] == 1:
-				pages = content["total_pages"]
-				for result in content["search_result"]:
-					try:
-						if not (result["domain_name"] in company_domains):
-							company_domains.append(result["domain_name"])
-						if not(result["company_name"].lower() in company_names):
-							print "[*] Added company name: '%s'" % result["company_name"]
-							
-						if not(result["email_address"].lower() in company_emails):
-							company_emails.append(result["email_address"])
-							
-					except:
-						continue
-			cur_page += 1
+def parse_whois(base_url, domain):
+    emails, companies, domains = {}, {}, {domain: False}
+    url = '{}&whois={}'.format(base_url, domain)
+    content = get(url).json()
+
+    logging.info('Performing WHOIS lookup on {}'.format(domain))
+
+    if (content['status'] != 1):
+        logging.error('WHOIS lookup failed, your API key is probably invalid or credits have been exhausted')
+        return {'emails': emails, 'companies': companies, 'domains': domains}
+    for record in ['registrant', 'administrative', 'technical']:
+        record = '{}_contact'.format(record)
+        email = content.get(record, {}).get('email_address', '').lower()
+        company = content.get(record, {}).get('company_name', '')
+        if email:
+            logging.debug('domain: adding email {}'.format(email))
+            emails[email] = True
+        if company:
+            logging.debug('domain: adding email {}'.format(email))
+            companies[company] = True
+    return {'emails': emails, 'companies': companies, 'domains': domains}
+
+
+def recursive_search(base_url, search, find, page=1, pages=9999):
+    emails, companies, domains = {}, {}, {}
+    while page <= pages:
+        url = '{}&reverse=whois&{}={}&mode=mini&page={}'.format(
+                base_url, search, find, page)
+        content = get(url).json()
+        total_pages = content.get('total_pages', 1)
+        if total_pages < pages:
+            pages = total_pages
+        if content.get('status', 0) == 1:
+            for result in content['search_result']:
+                domain = result.get('domain_name', '')
+                company = result.get('company_name', '')
+                email = result.get('email_address', '')
+                if domain:
+                    logging.debug('{}={} adding email {}'.format(search, find, domain))
+                    domains[domain] = True
+                if company:
+                    logging.debug('{}={} adding email {}'.format(search, find, company))
+                    companies[company] = True
+                if email:
+                    logging.debug('{}={} adding email {}'.format(search, find, email))
+                    emails[email] = True
+        page += 1
+    return {'domains': domains, 'emails': emails, 'companies': companies}
+
+
+def merge(source, destination):
+    for key, value in source.items():
+        if isinstance(value, dict):
+            # get node or create one
+            node = destination.setdefault(key, {})
+            merge(value, node)
+        else:
+            destination[key] = value
+    return destination
+
+
+def query_yes_no(question, default='yes'):
+    '''Ask a yes or no question'''
+    valid = {'yes': True, 'y': True, 'ye': True,
+            'no': False, 'n': False}
+    if default is None:
+        prompt = ' [y/n] '
+    elif default == 'yes':
+        prompt = ' [Y/n] '
+    elif default == 'no':
+        prompt = ' [y/N] '
+    else:
+        raise ValueError('invalid default answer: {}'.format(default))
+
+    while True:
+        choice = raw_input(question + prompt).lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            print('Please respond with \'yes\' or \'no\'(or \'y\' or \'n\').\n')
+
+
+def main():
+    args = get_args()
+    set_log_level(args.verbose)
+    api_key = args.api if args.api else read_key_from_config()
+    blacklist = {'domains': set(), 'emails': set(), 'companies': set()}
+    base_url = 'http://api.whoxy.com/?key={}'.format(api_key)
+
+    if '.' not in args.domain:
+        logging.error('It\'s probably unlikely that the target is a whole TLD')
+        exit()
+
+    results = parse_whois(base_url, args.domain)
+    while any(results['domains'].values() +
+            results['companies'].values() +
+            results['emails'].values()):
+        if args.domains:
+            for domain, check in results['domains'].items():
+                if not check:
+                    continue
+                check = query_yes_no('Do you want to check "{}"'.format(domain))
+                if check:
+                    results = merge(results, parse_whois(base_url, domain))
+                    check = False
+                else:
+                    blacklist['domains'].add(domain)
+                results['domains'][domain] = check
+        else:
+            results['domains'] = dict.fromkeys(results['domains'], False)
+        if args.companies:
+            for company, check in results['comainies'].items():
+                if not check:
+                    continue
+                check = query_yes_no('Do you want to check "{}"'.format(company))
+                if check:
+                    results = merge(results, recursive_search(base_url, 'company', company))
+                    check = False
+                else:
+                    blacklist['companies'].add(company)
+                results['companies'][company] = check
+        else:
+            results['companies'] = dict.fromkeys(results['companies'], False)
+        if args.emails:
+            for email, check in results['emails'].items():
+                if not check:
+                    continue
+                check = query_yes_no('Do you want to check "{}"'.format(email))
+                if check:
+                    results = merge(results, recursive_search(base_url, 'email', email))
+                    check = False
+                else:
+                    blacklist['emails'].add(email)
+                results['emails'][email] = check
+        else:
+            results['emails'] = dict.fromkeys(results['emails'], False)
+    output = '''
+### Company Names:
+{}\n
+### Domain Names:
+{}\n
+### Email Addresses:
+{}'''.format(
+            '\n'.join(
+                [k for k in results['companies'].keys() if k not in blacklist['companies']]),
+            '\n'.join(
+                [k for k in results['domains'].keys() if k not in blacklist['domains']]),
+            '\n'.join(
+                [k for k in results['emails'].keys() if k not in blacklist['emails']]))
+    print output
+    if args.output:
+        with open(args.output, 'w') as text_file:
+            text_file.write(output)
 
 
 if __name__ == '__main__':
-	banner()
-	if args.domain == None:
-		print "[!] No domain specified"
-		exit()
-	elif (not "." in args.domain):
-		print "[!] It's probably unlikely that the target is a whole TLD"
-		exit()
-
-	enum(args.domain)
-	expand()
-
-	if args.output != None:
-		file = open(args.output, 'w+')
-
-		file.write("[*] Company Names\r\n")
-		file.writelines([x+os.linesep for x in company_names])
-		
-
-		file.write("[*] Company Emails\r\n")
-		file.writelines([x+os.linesep for x in company_emails])
-
-		file.write("[*] Associated Domains\r\n")
-		file.writelines([x+os.linesep for x in company_domains])
-
-		file.close()
-
-		print "[*] Written to file %s" % args.output
-	else:
-		print "[*] Company Names"
-		for company_name in company_names:
-			print company_name
-
-		print "[*] Company Emails"
-
-		for company_email in company_emails:
-			print company_emails
-
-		print "[*] Associated Domains"
-
-		for domain in company_domains:
-			print domain
-
+    main()
